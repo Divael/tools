e573ebb5fd4622ce830544923aa7350083ec0f38
海 杨 <yangsea101@outlook.com>
2020-04-24 20:26:51 +0800

    添加了tool  INotifyPropertyChanged接口的实现

---
+++ Tools/Tools/mvvm/PropertyChangedBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.mvvm
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 属性更改通知事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// 引发属性更改通知事件
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        public void NotifyPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            if (PropertyChanged == null)
+                return;
+
+            var memberExpression = property.Body as MemberExpression;
+            if (memberExpression == null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(memberExpression.Member.Name));
+        }
+
+    }
+}
