64c2af4c1490f79b6088d04f59d9d705b1d6b3dd
YangHai <yangsea101@qq.com>
2020-07-14 10:00:54 +0800

    1

---
+++ Tools/异步并行async await patalled用法/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 异步并行async_await_patalled用法
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //泛型写法
+            BeginInvoke(new Action(async ()=> {
+                int Count = 100;
+                await Task.Run(()=> {
+                    Parallel.For(0,Count,new ParallelOptions() {MaxDegreeOfParallelism = 8 },(i,state)=> { 
+                        
+                    });
+                });
+            }));
+        }
+    }
+}
